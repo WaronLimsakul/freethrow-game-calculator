2ecddcee1be0aadace6bf31534493989a285e8bd
Waron Limsakul <poonro@gmail.com>
2024-11-23 21:51:49 -0500

    add comment and readme

--- freethrow_turn.py
+++ freethrow_turn.py
@@ -1,15 +1,17 @@
 import numpy as np
 import sys
 
-a_fg = float(sys.argv[1])
-b_fg = float(sys.argv[2])
-goal = int(sys.argv[3])
+a_fg = float(sys.argv[1])  # first argument is first player's scoring probability
+b_fg = float(sys.argv[2])  # secodn argument is second player's scoring probability
+goal = int(sys.argv[3])  # how many shot it takes to win.
 if len(sys.argv) == 5:
+    # you set put how many times you want to simulate (auto 100000 times)
     simulations = int(sys.argv[4])
 else:
     simulations = 100000
 
 
+# function for simulate one game.
 def simulate_game(a_fg, b_fg, goal):
     a = b = 0
     turn_a = True
@@ -32,10 +34,12 @@ def simulate_game(a_fg, b_fg, goal):
 a_count = b_count = 0
 
 
+# iterate each game
 for _ in range(simulations):
     if simulate_game(a_fg, b_fg, goal) == "A":
         a_count += 1
     else:
         b_count += 1
+
 print(f"a winning chance: {a_count * 100 / simulations}%")
 print(f"b winning chance: {b_count * 100 / simulations}%")
---
+++ readme.md
@@ -0,0 +1,31 @@
+# freethrow game calculator
+
+this is a repo I build purely out of my curiosity
+
+## Rules
+
+There are many variations of this game, but there are two popular variations
+
+1. turn-based, player takes to shoot until one of them, the winner, reach the goal.
+2. 1 turn, one player shoot until he reaches the goal, then another player. Whoever
+   use the fewer shots win.
+
+## Purpose
+
+This tool is used for calcuating the probability of one player to win over the other.
+
+## Usage
+
+1. Clone this repo
+2. Install numpy
+3. run the variation user wants
+
+### Arguments
+
+the code must be given these 3 arguments.
+
+- first one is a scoring probability of first player "a"
+- the second is that of "b"
+- the third is how scoring goal of this game.
+
+\*\* (optional) The last argument is number of simulations.
